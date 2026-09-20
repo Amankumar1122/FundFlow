3b562447d9a91ae2a3d648aabff7d37e4e69e1e4
Amankumar1122 <bgmia@Aman_PC>
2026-09-20 22:00:10 +0530

    Replace backend with Spring Boot project

---
+++ fundflow-backend/src/test/java/FundFlow/FundflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package FundFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
